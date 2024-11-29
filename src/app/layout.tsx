a6c6019bd22ed04f2914553985e723a0512e47b2
nyraa <112930946+nyraa@users.noreply.github.com>
2024-11-30 01:22:58 +0800

    remove unused meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,6 @@ export default function RootLayout({
 {
     return (
         <html lang="en">
-            <head>
-                <link rel="icon" href="/favicon.ico" sizes="any" />
-            </head>
             <body>
                 {children}
             </body>
