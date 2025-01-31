f2888cce98d9e664ed1c78d600fe2d9f9897817e
nyraa <112930946+nyraa@users.noreply.github.com>
2025-01-31 11:26:33 +0800

    update icons style

--- src/app/components/InfoDialog.tsx
+++ src/app/components/InfoDialog.tsx
@@ -2,8 +2,9 @@
 
 import { useState } from "react";
 import { Dialog, DialogTitle, DialogContent, IconButton, Typography, Link, Box } from "@mui/material";
-import InfoIcon from "@mui/icons-material/Info";
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import GitHubIcon from "@mui/icons-material/GitHub";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
 export default function InfoDialog()
 {
@@ -12,21 +13,23 @@ export default function InfoDialog()
     return (
         <>
             <IconButton sx={{
-                ml: 2
+                ml: 2,
+                color: "black"
             }} onClick={() => setOpen(true)} color="primary">
-                <InfoIcon />
+                <InfoOutlinedIcon />
             </IconButton>
 
             <Dialog open={open} onClose={() => setOpen(false)}>
-                <DialogTitle>About</DialogTitle>
+                <DialogTitle>色.tw</DialogTitle>
                 <DialogContent>
                     <Typography variant="body1">
                         閒著沒事就買了<Link href="https://色.tw">色.tw</Link>
                     </Typography>
                     <Box display="flex" alignItems="center" mt={1}>
                         <GitHubIcon sx={{ mr: 1 }} />
-                        <Link href="https://github.com/nyraa/color-palette" target="_blank" rel="noopener">
+                        <Link display="inline-flex" alignItems="center" href="https://github.com/nyraa/color-palette" target="_blank" rel="noopener">
                             Source code
+                            <OpenInNewIcon sx={{ ml: 0.5, fontSize: "1em" }} />
                         </Link>
                     </Box>
                 </DialogContent>
